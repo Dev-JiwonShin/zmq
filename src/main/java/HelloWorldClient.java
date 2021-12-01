4825ff7e102a82e571c091a2b96dd1fe896002a7
JiwonShin <dev.jiwonshin@gmail.com>
2021-12-01 21:51:44 +0900

    First commit

---
+++ src/main/java/HelloWorldClient.java
@@ -0,0 +1,37 @@
+// package com.imatix.zguide.ex1_4;
+
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
+import java.util.stream.IntStream;
+
+import static java.lang.String.format;
+import static org.zeromq.ZMQ.REQ;
+import static org.zeromq.ZMQ.context;
+
+/**
+ * Hello World client in Java
+ * Connects REQ socket to tcp://*:5555
+ * Sends "Hello" to server, expects "World"
+ *
+ * @since 1.0
+ */
+
+public class HelloWorldClient {
+    
+    public static void main(String[] args) {
+        Context context = context(10);
+        // Socket to talk to Server
+        Socket requester = context.socket(REQ);
+        requester.connect(Constants.URL.TCP);
+        IntStream.range(0, 10)
+                .forEach(reqNum -> {
+                    System.out.println(format("Sending Hello %s", reqNum));
+                    requester.send("Hello".getBytes(), 0);
+                    byte[] reply = requester.recv(0);
+                    System.out.println(format("Received %s : %s", new String(reply), reqNum));
+                });
+        requester.close();
+        context.term();
+    }
+}
